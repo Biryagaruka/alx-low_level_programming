41302a194641b21434299f7c28ccb157a9bf7cc5
Biryagaruka <ianzkert@gmail.com>
2023-02-16 16:43:39 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+ putss("\"Programming is like building a multilingual puzzle" );
+ return (0);
+}
