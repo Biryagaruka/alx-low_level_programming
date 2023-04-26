e6e273382f76a8da5f7b29ab6d950750307b6176
Biryagaruka <ianzkert@gmail.com>
2023-04-26 00:57:14 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,24 +4,24 @@
  * @b: const char
  * Return: 0
  */
-unsigned int binary_to_uint(const char *p)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
 	int base = 1, i = 0;
 
-	if (p == NULL)
+	if (b == NULL)
 		return (0);
 
-	while (p[i + 1])
+	while (b[i + 1])
 	{
-		if (p[i] != '0' && p[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
 
 	while (i >= 0)
 	{
-		res += ((p[i] - '0') * base);
+		res += ((b[i] - '0') * base);
 		base *= 2;
 		i--;
 	}
